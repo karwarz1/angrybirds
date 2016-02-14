950e0e89f54fcf331865cda1885d9137792c7921
pizzadeesnutz <arcadiomj@yahoo.com>
2016-02-14 18:56:44 -0500

    According to the book I'm done
    I feel like it is missing something, I also don't like the walls being
    white because they are hard to see in contrast to the clouds. I feel
    like the clouds are in the wrong layer or something, so I may recolor
    and fix some things to tweak this to how I want it.

--- Assets/__Scripts/Slingshot.cs
+++ Assets/__Scripts/Slingshot.cs
@@ -47,6 +47,7 @@ public class Slingshot : MonoBehaviour {
             projectile.GetComponent<Rigidbody>().velocity = -mouseDelta * velocityMult;
             FollowCam.S.poi = projectile;
             projectile = null;
+            MissionDemolition.ShotFired();
         }
 	}
 
